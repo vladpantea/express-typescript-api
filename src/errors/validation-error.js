0e1ed6df42519727a99b4f9f3f6bbc6990ba1dff
Vlad Pantea <vlad.pantea@gmail.com>
2019-09-10 09:07:08 +0300

    code clean

--- src/errors/validation-error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ValidationError {
-    constructor(message) {
-        this.message = message;
-    }
-}
-exports.default = ValidationError;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmFsaWRhdGlvbi1lcnJvci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInZhbGlkYXRpb24tZXJyb3IudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQSxNQUFNLGVBQWU7SUFHakIsWUFBWSxPQUFjO1FBQ3RCLElBQUksQ0FBQyxPQUFPLEdBQUcsT0FBTyxDQUFDO0lBQzNCLENBQUM7Q0FDSjtBQUVELGtCQUFlLGVBQWUsQ0FBQyJ9
